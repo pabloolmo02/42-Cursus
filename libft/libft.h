61da11419730d62e38cd20034456dcc17370aec5
Pablo Olmo Lopez <polmo-lo@c1r12s1.42malaga.com>
2024-04-15 14:03:47 +0200

    MiLibreria

---
+++ libft/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: polmo-lo <polmo-lo@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 13:57:32 by polmo-lo          #+#    #+#             */
+/*   Updated: 2024/04/15 14:03:02 by polmo-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBF_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int 	ft_isascii(int c);
+int		ft_isprint(int c);
+
+#endif
