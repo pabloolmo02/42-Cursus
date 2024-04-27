045ecd87a88f23d0403d23288cbee6027529764a
Pablo Olmo Lopez <polmo-lo@c1r4s6.42malaga.com>
2024-04-27 20:15:07 +0200

    libreria

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -1,18 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: polmo-lo <polmo-lo@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/15 12:54:31 by polmo-lo          #+#    #+#             */
-/*   Updated: 2024/04/27 13:57:44 by polmo-lo         ###   ########.fr       */
+/*   Created: 2024/04/27 15:47:15 by polmo-lo          #+#    #+#             */
+/*   Updated: 2024/04/27 15:47:38 by polmo-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isdigit(int c)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	return (c >= '0' && c <= '9');
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!dest || !src)
+		return (0);
+	while (src[i])
+		i++;
+	while (src(j) && j + 1 < size)
+		dest[i] = src[i];
+	i++;
+	if (size)
+		dest[i] = '\0';
+	return (i);
 }
